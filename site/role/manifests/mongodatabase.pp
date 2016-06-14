581adc9e2572c622746b0c681e522cda36cd6dd6
ailiite <ahowell@singularity.ltd.uk>
2016-06-14 16:55:45 +0100

    WIP

--- site/role/manifests/mongodatabase.pp
+++ site/role/manifests/mongodatabase.pp
@@ -1,3 +1,28 @@
 class role::mongodatabase {
-    include ::profile::mongodatabase
+ # Use 10gen repositories instead of distribution's
+  class { 'mongodb::globals':
+    manage_package_repo => true,
+    version             => $ver
+  }->
+
+  class { 'mongodb::client':
+    require => Class['Mongodb::globals']
+  }
+
+  class { 'mongodb::server':
+    ensure  => present,
+    port    => 27017,
+    verbose => true,
+    auth    => true,
+    journal => true,
+    rest    => false,
+    require => [Class['Mongodb::client']]
+  }
+
+  package { ['mongodb-org-tools']:
+    ensure => present,
+    before => Class['mongodb::server']
+  }
+
+
 }
